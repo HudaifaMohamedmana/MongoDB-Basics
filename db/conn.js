3df5dc8fee839215df20410c43a5f7101660b288
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-09-10 17:43:21 -0400

    it workes

--- db/conn.js
+++ db/conn.js
@@ -1,27 +1,22 @@
-const dotenv = require('dotenv');
 const { MongoClient } = require('mongodb');
+
+const dotenv = require('dotenv');
 dotenv.config();
 
 const connectionString = process.env.ATLAS_URI || '';
 const client = new MongoClient(connectionString);
 
-let conn;
 let db;
 
-(async () => {
+const dbconnect = async () => {
     try {
-        conn = await client.connect();
-        db = conn.db('sample_training');
+        await client.connect();
+        db = client.db('sample_training');
         console.log('Connected to MongoDB');
+        return db;
     } catch (e) {
         console.error('Error connecting to MongoDB:', e);
     }
-})();
+};
 
-process.on('SIGINT', async () => {
-    await client.close();
-    console.log('MongoDB connection closed');
-    process.exit(0);
-});
-
-module.exports = { db };
+module.exports = dbconnect;
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const db = require("../db/conn.mjs");
-
+const dbconnect = require("../db/conn.js");
 const router = express.Router();
-console.log(db)
-// Define a route
-// router.get("/", async (req, res) => {
-//   let collection = await db.collection("post")
-//   let resulte = await collection.find({}).limit(50).toArray();
-//   res.send(resulte).status(200);
-// });
+
+
+// console.log(db)
+router.get("/", async (req, res) => {
+    let db = await dbconnect()
+    let collection = await db.collection("grades")
+    let resulte = await collection.find({}).limit(50).toArray();
+    res.send(resulte).status(200);
+});
 
 // Export the router
 module.exports = router;
